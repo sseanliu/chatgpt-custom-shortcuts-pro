cd0a20fd0d95ceecb0f72dfd8081658d24286487
Brian Hurd <bwhurd@gmail.com>
2025-06-05 01:15:02 -0500

    Merge popup.html changes

--- 3.3.7.0/background.js
+++
@@ -1,9 +0,0 @@
-// Import the chrome.runtime module
-const { runtime } = chrome;
-
-// Listen for keyboard shortcut command
-chrome.commands.onCommand.addListener((command) => {
-    if (command === "open-popup") {
-        chrome.action.openPopup();
-    }
-});
